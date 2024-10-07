43ecb696233f6437c675a8b599c2f4d5340e92bf
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-10-07 16:20:17 -0300

    FEAT: Adding the Get ENV Token Function to ./main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd # For reading CSV files
 import platform # For getting the operating system name
 import sys # For exiting the program
 from colorama import Style # For coloring the terminal
+from dotenv import load_dotenv # For loading environment variables from .env file
 
 # Macros:
 class BackgroundColors: # Colors for the terminal
@@ -17,6 +18,10 @@ class BackgroundColors: # Colors for the terminal
 
 # Execution Constants:
 VERBOSE = False # Verbose mode. If set to True, it will output messages at the start/call of each function (Note: It will output a lot of messages).
+
+# .Env Constants:
+ENV_PATH = f".env" # The path to the .env file
+ENV_VARIABLE = f"GITHUB_TOKEN" # The environment variable to load
 
 # Input Constants:
 INPUT_DIRECTORY = f"./Inputs/" # The path to the input directory
@@ -51,6 +56,30 @@ def verify_filepath_exists(filepath):
    verbose_output(f"{BackgroundColors.YELLOW}Verifying if the file or folder exists at the path: {BackgroundColors.CYAN}{filepath}{Style.RESET_ALL}") # Output the verbose message
 
    return os.path.exists(filepath) # Return True if the file or folder exists, False otherwise
+
+def get_env_token(env_path=ENV_PATH, key=ENV_VARIABLE):
+   """
+   Verify if the .env file exists and if the desired key is present.
+
+   :param env_path: Path to the .env file.
+   :param key: The key to get in the .env file.
+   :return: The value of the key if it exists.
+   """
+
+   verbose_output(true_string=f"{BackgroundColors.GREEN}Verifying the .env file...{Style.RESET_ALL}")
+
+   if not verify_filepath_exists(env_path): # Verify if the .env file exists
+      print(f"{BackgroundColors.RED}The {BackgroundColors.CYAN}.env file{BackgroundColors.RED} not found at {BackgroundColors.CYAN}{env_path}{Style.RESET_ALL}")
+      sys.exit(1) # Exit the program
+
+   load_dotenv(env_path) # Load the .env file
+   api_key = os.getenv(key) # Get the value of the key
+
+   if not api_key: # Verify if the key exists
+      print(f"{BackgroundColors.RED}The {BackgroundColors.CYAN}{key}{BackgroundColors.RED} key was not found in the .env file located at {BackgroundColors.CYAN}{env_path}{Style.RESET_ALL}")
+      sys.exit(1) # Exit the program
+
+   return api_key # Return the value of the key
 
 def read_csv_file():
    """
@@ -91,6 +120,8 @@ def main():
    """
 
    print(f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}AIs API Response Collector{BackgroundColors.GREEN}!{Style.RESET_ALL}\n\n") # Output the welcome message
+
+   get_env_token() # Get the API token from the .env file
 
    print(f"\n{BackgroundColors.BOLD}{BackgroundColors.GREEN}Program finished.{Style.RESET_ALL}") # Output the end of the program message
    atexit.register(play_sound) # Register the function to play a sound when the program finishes
