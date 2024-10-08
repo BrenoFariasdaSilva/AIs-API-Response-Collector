80f71cd794f40f2b5d9ca61bffc0a47bd74d70c7
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-10-08 17:15:17 -0300

    REFACTOR: Improving the Main Function in ./copilot.py

--- copilot.py
+++ copilot.py
@@ -1,6 +1,9 @@
+import atexit # For playing a sound when the program finishes
 import os # For running a command in the termina
+from colorama import Style # For coloring the terminal
+from utils import BackgroundColors # Import Classes from ./utils.py
 from utils import OUTPUT_DIRECTORY # Import Constants from ./utils.py
-from utils import create_directory, write_output_to_file # Import Functions from ./utils.py
+from utils import create_directory, play_sound, write_output_to_file # Import Functions from ./utils.py
 
 class CopilotModel:
 	"""
@@ -33,10 +36,17 @@ def main():
 	:return: None
 	"""
 
+	print(f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}GitHub Copilot AI Model{BackgroundColors.GREEN}!{Style.RESET_ALL}\n\n") # Output the welcome message
+
 	copilot = CopilotModel() # Create the CopilotModel object
-	output = copilot.run() # Run the CopilotModel
+	task_message = "Explain how to create a new branch in Git." # The task message
+	task_type = "explain" # The task type
+	output = copilot.run(task_message, task_type) # Run the CopilotModel
 	create_directory(os.path.abspath(OUTPUT_DIRECTORY), OUTPUT_DIRECTORY.replace(".", "")) # Create the output directory
 	write_output_to_file(output, CopilotModel.OUTPUT_FILE) # Write the output to the file
+
+	print(f"{BackgroundColors.BOLD}{BackgroundColors.GREEN}Program finished.{Style.RESET_ALL}") # Output the end of the program message
+	atexit.register(play_sound) # Register the function to play a sound when the program finishes
 
 if __name__ == "__main__":
    """
