56488dfc1aece8243c0af1a8942b090e96f2151c
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-10-08 15:33:32 -0300

    REFACTOR: Importing the BackgroundColors Class in ./main.py from ./utils.py

--- main.py
+++ main.py
@@ -5,16 +5,7 @@ import platform # For getting the operating system name
 import sys # For exiting the program
 from colorama import Style # For coloring the terminal
 from gemini import GeminiModel # Import the GeminiModel class from gemini.py
-
-# Macros:
-class BackgroundColors: # Colors for the terminal
-   CYAN = "\033[96m" # Cyan
-   GREEN = "\033[92m" # Green
-   YELLOW = "\033[93m" # Yellow
-   RED = "\033[91m" # Red
-   BOLD = "\033[1m" # Bold
-   UNDERLINE = "\033[4m" # Underline
-   CLEAR_TERMINAL = "\033[H\033[J" # Clear the terminal
+from utils import BackgroundColors # Import Classes from utils.py
 
 # Execution Constants:
 VERBOSE = False # Verbose mode. If set to True, it will output messages at the start/call of each function (Note: It will output a lot of messages).
