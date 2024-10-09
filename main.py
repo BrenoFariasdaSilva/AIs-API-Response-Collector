1864b46b1972b6a47d7c97c1d5a5777d8b4a3810
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-10-08 21:46:50 -0300

    REFACTOR: Importing the ChatGPTModel Class to ./main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os # For running a command in the terminal
 import pandas as pd # For reading CSV files
 import sys # For exiting the program
 from colorama import Style # For coloring the terminal
+from chatgpt import ChatGPTModel # Import the ChatGPTModel class from chatgpt.py
 from copilot import CopilotModel # Import the CopilotModel class from copilot.py
 from gemini import GeminiModel # Import the GeminiModel class from gemini.py
 from utils import BackgroundColors # Import Classes from ./utils.py
@@ -10,7 +11,7 @@ from utils import START_PATH, OUTPUT_DIRECTORY # Import Constants from ./utils.p
 from utils import create_directory, play_sound, verbose_output # Import Functions from ./utils.py
 
 # Execution Constants:
-EXECUTE_MODELS = {"Copilot": "CopilotModel", "Gemini": "GeminiModel"} # The AI models to execute
+EXECUTE_MODELS = {"chatgpt": "ChatGPTModel", "Copilot": "CopilotModel", "Gemini": "GeminiModel"} # The AI models to execute
 
 # Input/Output Directory Constants:
 INPUT_DIRECTORY = f"{START_PATH}/Inputs/" # The path to the input directory
